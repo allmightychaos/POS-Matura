9d6e57c5c35003d0ba1d5ba621b1a47fd7f60292
Allmightychaos <85056161+allmightychaos@users.noreply.github.com>
2026-04-25 14:44:01 +0200

    Add SimpleThreadApp project

---
+++ SimpleThreadApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace SimpleThreadApp
+{
+    class SimpleThreadApp
+    {
+        static int counter = 0;
+        static object lockObj = new object();
+
+        public static void ThreadWorker(int id, int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                if (counter % id == 0)
+                {
+                    Console.WriteLine("ID: {0,3} Counter: {1,8} Modulo: {2}", id, counter, counter % id);
+                }
+
+                lock (lockObj)
+                {
+                    counter++;
+                }
+            }
+        }
+
+        public static void OldMain(string[] args)
+        {
+            int threadCounter = int.Parse(args[0]);
+            int iterCounter = int.Parse(args[1]);
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < threadCounter; i++)
+            {
+                int id = i + 2;
+                Thread t = new Thread(() => ThreadWorker(id, iterCounter));
+                threads.Add(t);
+
+                t.Start();
+            }
+
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+        }
+
+    }
+}
